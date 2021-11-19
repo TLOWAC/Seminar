bd6062b72b64a7d02bf14a901dee0ecb4d6abdf0
chlee-danbi <ch.lee@danbicorp.com>
2021-11-19 21:07:20 +0900

    feat: slidev init

---
+++ slidev/components/Image.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const example1 = new Vue({
+  data: {
+    images: [
+      "https://source.unsplash.com/collection/94734566/1920x1080",
+      "https://source.unsplash.com/collection/94734566/1920x1080",
+      "https://source.unsplash.com/collection/94734566/1920x1080",
+    ],
+  },
+});
+
+const images = ref(props.images);
+</script>
+
+<template>
+  <div flex="~" w="min" border="~ gray-400 opacity-50 rounded-md">
+    <ul id="example1">
+      <li v-for="image in images">
+        {{ image }}
+      </li>
+    </ul>
+  </div>
+</template>
